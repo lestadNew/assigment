cb9f7d96322e7684596cfa436e5c414ac52f263c
Kovryhin Dmytro <dima.k@uklon.com.ua>
2020-03-11 12:49:58 +0200

    Initial Commit

---
+++ TestAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAssignment
+//
+//  Created by Dmytro Kovryhin on 11.03.2020.
+//  Copyright © 2020 Dmytro Kovryhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
